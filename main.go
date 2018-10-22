fd6e927262b1d344250fd380efcf8ee1af7469e3
singcl <24661881@qq.com>
2018-10-22 15:39:57 +0800

    use package go-qrcode generage qrcode

--- main.go
+++ main.go
@@ -6,12 +6,17 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/astaxie/beego"
-	simplejson "github.com/bitly/go-simplejson"
+	"github.com/bitly/go-simplejson"
 	"github.com/singcl/go-dep-glide/config"
+	"github.com/skip2/go-qrcode"
+	"log"
+	"image/color"
 )
 
 func main() {
 	configContent()
+	generateQrcode()
+
 }
 
 // 启动beego服务器
@@ -26,10 +31,26 @@ func runBeego() {
 
 // 测试config包读取的内容
 func configContent() {
-	var config config.TomlConfig
+	var con config.TomlConfig
 	filePath := "conf.toml"
-	if _, err := toml.DecodeFile(filePath, &config); err != nil {
+	if _, err := toml.DecodeFile(filePath, &con); err != nil {
 		panic(err)
 	}
-	fmt.Printf("conf.toml的内容为：%v", config)
+	fmt.Printf("conf.toml的内容为：%v", con)
+}
+
+// 生成二维码
+func generateQrcode() {
+	// 快捷方式生成二维码
+	//qrcode.WriteFile("https://baidu.com", qrcode.Medium, 256, "baidu.png")
+
+	// 自定义二维码
+	qr, err := qrcode.New("https://baidu.com", qrcode.Medium)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		qr.BackgroundColor = color.RGBA{50, 205, 50, 255 }
+		qr.ForegroundColor = color.White
+		qr.WriteFile(256, "./baidu.png")
+	}
 }
